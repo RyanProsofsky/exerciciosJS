13c7e7c1ccafe4f04b19d51c66b3ee0cb7c1c707
Ryan Prosofsky <93451770+RyanProsofsky@users.noreply.github.com>
2024-01-31 17:25:22 -0300

    feito

--- exercicios/exercicios.js
+++ exercicios/exercicios.js
@@ -1,36 +1,36 @@
-//1 - Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo. 
+//1 - Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo.
 /*
 let base = prompt('Digite o valor da base');
-let altura = prompt('Digite o valor da altura')  ;                               
+let altura = prompt('Digite o valor da altura');                               
 let total = base * altura
 
 console.log(O valor da área é de: ${total});
 */
 
-
-
 //2 - Verifique se um número é primo: Crie um programa que determine se um número é primo ou não. (Um número primo é aquele que é dividido apenas por um e por ele mesmo. Entre 0 e 100 existem apenas 25 números primos.)
 
-
-
-
-
 //3 - Calcule o fatorial de um número: Escreva um programa que calcule o fatorial de um número dado.
+let valor = 7
 
 
+while(valor >= 1 ){
+    console.log(valor)
+    valor--
+}
 
 
+//4 - Escreva um algoritmo para ler o salário mensal atual de um funcionário e o percentual de reajuste. Calcular e escrever o valor do novo salário.
+/*
+let nomeFuncionario = prompt('Digite o nome do funcionário: ');
+let salarioFixo = prompt('Digite o sálario: R$');
+let percentualReajuste = prompt('Digite o percentual de reajuste: ');
+let contaPercentual = (percentualReajuste * salarioFixo) / 100
+let salarioFinal =  parseFloat(contaPercentual) + parseFloat(salarioFixo);
 
-//4 - Escreva um algoritmo para ler o salário mensal atual de um funcionário e o percentual de reajuste. Calcular e escrever o valor do novo salário. 
-let nomeFuncionario = prompt('Digite o nome do funcionário: ')
-let salarioMensal = prompt('Digite o sálario: R$')
-let percentualReajuste = prompt('Digite o percentual de reajuste: ')
-let salarioFinal 
+console.log(`O salario com reajuste é de: R$${salarioFinal}`);
+*/
 
-
-
-
-//5 - Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles. 
+//5 - Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles.
 /*
 let n1 = prompt('Digite o primeiro valor: ');
 let n2 = prompt('Digite o segundo valor: ');
@@ -42,7 +42,6 @@ if(n1 > n2){
 }
 */
 
-
 //6. Construa um algoritmo que calcule o valor do comprimento da circunferência, a partir do valor do raio.
 /*
 let valorRaio = prompt('Digite o valor do raio: ');
@@ -52,7 +51,6 @@ console.log(valorConta);
 
 */
 
-
 //7. Desenhe um algoritmo que receba dois numeros e exiba o resultado da sua soma.
 /*
 let n1 = prompt('Digite o primeiro valor: ');
@@ -61,7 +59,6 @@ let total = parseInt(n1) + parseInt(n2);
 
 alert('O valor total é de ' + total);
 */
-
 
 //8. Desenhe um algoritmo que receba dois numeros e mostre a soma, a subtracao, a multiplicação e a divisao dos numeros.
 /*
@@ -78,19 +75,30 @@ console.log(resultadoDivisao);
 console.log(resultadoMultiplicacao);
 */
 
+//9. Desenhe um algoritmo para determinar o consumo medio de um automovel, considerando que a distancia total percorrida e o total de combustivel gasto sao fornecidos.
+/*
+let combustivelGasto = prompt('Digite quantos litros de combustivel foi abastecido:  ')
+let distanciaTotal = prompt('Digite a distancia percorrida: ')
+let consumoMedio =parseFloat(distanciaTotal) / parseFloat(combustivelGasto) 
+
+//let combustivelGasto = '20.45265296519415161988789'
+//let distanciaTotal = '30.546'
+// console.log(parseFloat(combustivelGasto).toFixed(6))
 
 
-
-
-//9. Desenhe um algoritmo para determinar o consumo medio de um automovel, considerando que a distancia total percorrida \ne o total de combustivel gasto sao fornecidos.
-
-
-
+console.log(`O consumo médio é de: ${consumoMedio.toFixed(2)}km `)
+*/
 
 //10. Elabore um algoritmo que leia o nome de um vendedor, o seu salario fixo e o total de vendas efetuadas por ele no mes (em dinheiro). Sabendo que esse vendedor ganha 15 de comissao sobre suas vendas efetuadas, faça que o algoritmo informe o seu nome, o salario fixo e salario no final do mês.
+/*
+let nomeVendedor = prompt('Escreva o nome do vendedor: ');
+let salarioFixo = prompt('Digite o salário R$: ');
+let totalVendas = prompt('Digite o total de vendas R$: ');
+let porcentagem = (totalVendas * 15) / 100
+let salarioFinal = parseFloat(porcentagem) + parseFloat(salarioFixo)
 
-
-
+console.log(salarioFinal)
+*/
 
 //11. Crie um algoritmo que leia o nome de um aluno e as notas das tres provas que ele obteve no semestre, com pesos 2, 3 e 5. No final, deve-se informar o nome do aluno e a sua média ponderada.
 /*
@@ -109,22 +117,17 @@ let totalPeso = p1 + p2 + p3
 let resultadoFinal = total / totalPeso
 
 
-console.log(`A média ponderada do ${nomeAluno} é ${resultadoFinal}`)
+console.log(`A média ponderada do ${nomeAluno} é ${resultadoFinal}`);
 */
-
-
-
 
 //12. Desenhe um algoritmo que leia uma temperatura em graus Celsius e a apresente convertida em graus Fahrenheit. \nA formula de conversao: F = (9 * C + 160) / 5, sendo F a temperatura em graus Fahrenheit e C, a temperatura em graus Celsius.
 /*
 let grausCelsius = prompt('Digite a temperatura °C: ');
 let conversaoGraus = ((9 * grausCelsius) + 160 ) / 5
 
-console.log(`O valor da temperatura em °f é de: ${conversaoGraus}`)
+console.log(`O valor da temperatura em °f é de: ${conversaoGraus}`);
 
 */
-
-
 
 //13. Elabore um algoritmo que efetue a apresenta  o do valor da conversao em real (RS) de um valor lido em dolar (USS). O algoritmo dever  solicitar o valor da cotacao do dolar e tambem a quantidade de dolares disponiveis com o usuário.
 /*
@@ -136,10 +139,14 @@ console.log(`O valor da conversão foi de: ${conversao}`);
 
 */
 
+//14. Faca um algoritmo que calcule o rendimento de um deposito apos um mes de aplicacao. Considere fixa a taxa de juros de remuneracao, correspondente a 0,50% ao mes
+/*
+let aplicacao = prompt('Escreva o valor da aplicação R$:')
+let rentabilidade =  (aplicacao * 0.50) / 100 
 
-//14. Faca um algoritmo que calcule o rendimento de um deposito apos um mes de aplicacao. Considere fixa a taxa de juros de remuneracao, correspondente a 0,50% ao mes 
+console.log(`A rentabilidade foi de:${rentabilidade}`)
 
-
+*/
 
 //15. Faca um algoritmo que receba um numero e mostre uma mensagem caso este seja maior que 10.
 /*
@@ -152,17 +159,12 @@ let valor = prompt('Digite o valor: ');
  }
 */
 
-
 //16. Crie uma contagem crescente até 10 a partir do 1.
 /*
 for(i = 0; i <= 10; i++){
     console.log(i);
 }
 */
-
-
-
-
 
 //17. Crie uma contagem decrescente de 10 até 1.
 /*
@@ -172,15 +174,17 @@ for(i = 10; i >= 1; i--){
 */
 
 //18. Crie a tabuada do 7.
-
-
+/*
+for(i = 1; i <= 10; i++){
+    console.log(7 * i)
+    //console.log(i);
+}
+*/
 
 //19. Crie um validador de Senha.
 
 
-
 //20. Crie um Advinhador do numero secreto.
-
 
 
 //21. Digite a altura de 5 jogadores de um time  de basquete e mostre a altura do menor e do maior jogador
